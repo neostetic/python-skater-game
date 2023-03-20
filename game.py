291305b424cb1392d12150f1a01dc1de606df4b6
jan.polacek <gg.polacek@gmail.com>
2023-03-20 15:22:37 +0100

    Initial

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 from entity.entity import Entity
 
+import pygame
+
 
 class Game:
-    def __init__(self, pygame, scaler, width, height, isFullscreen):
-        self.pygame = pygame
+    pygame = pygame
+
+    def __init__(self, scaler, width, height, isFullscreen):
         self.win = None
         self.scaler = scaler
         self.width = width
@@ -18,7 +21,7 @@ class Game:
         self.windowBorder = number
 
     def drawEntity(self, entity):
-        img = self.pygame.transform.scale(entity.image, (entity.width * self.scaler, entity.height * self.scaler))
+        img = Game.pygame.transform.scale(entity.image, (entity.width * self.scaler, entity.height * self.scaler))
         self.win.blit(img, (entity.x, entity.y))
 
     def moveEntity(self, entity, speed, threshold, reset):
@@ -32,49 +35,64 @@ class Game:
         self.drawEntity(entity)
 
     def drawText(self, string, font, color, x, y):
-        self.pygame.font.init()
+        Game.pygame.font.init()
         entity = font.render(string, False, color)
         self.win.blit(entity, (x, y))
 
-    def start(self):
-        self.pygame.init()
+    def keyhandlerMoveEntity(self, entity, velocity, threshold_entity):
+        velocity = velocity * self.scaler
+        if self.keys[Game.pygame.K_a] and entity.x > 0:
+            entity.x -= velocity * 1.5
+        elif self.keys[Game.pygame.K_d] and entity.x < self.width - entity.width * self.scaler:
+            entity.x += velocity
+        if self.keys[Game.pygame.K_w] and entity.y > self.height - threshold_entity.height * self.scaler - entity.height * self.scaler * .75:
+            entity.y -= velocity
+        elif self.keys[Game.pygame.K_s] and entity.y < self.height - entity.height * self.scaler:
+            entity.y += velocity
 
-        self.keys = self.pygame.key.get_pressed()
+    def start(self):
+        Game.pygame.init()
+
+        self.keys = Game.pygame.key.get_pressed()
         if self.isFullscreen:
-            self.win = self.pygame.display.set_mode((self.width, self.height), self.pygame.FULLSCREEN)
+            self.win = Game.pygame.display.set_mode((self.width, self.height), Game.pygame.FULLSCREEN, vsync=1)
         else:
-            self.win = self.pygame.display.set_mode((self.width, self.height))
-        skater = Entity(self.pygame, "assets/p1_default.png", 0, 0)
-        driveway = Entity(self.pygame, "assets/bg_street_road_v2.png", self.scaler, 0)
+            self.win = Game.pygame.display.set_mode((self.width, self.height), vsync=1)
+        skater = Entity(Game.pygame, "assets/p1_default.png", 0, 0)
+        skater.x = skater.width * self.scaler
+        skater.y = self.height - skater.height * self.scaler * 2
+        skate = Entity(Game.pygame, "assets/sk_default.png", 0, 0)
+        driveway = Entity(Game.pygame, "assets/bg_street_road_v2.png", self.scaler, 0)
         driveway.y = self.height - driveway.height * self.scaler
-        drivewaySecond = Entity(self.pygame, "assets/bg_street_road_v2.png", 0, 0)
+        drivewaySecond = Entity(Game.pygame, "assets/bg_street_road_v2.png", 0, 0)
         drivewaySecond.y = self.height - driveway.height * self.scaler
-        background = Entity(self.pygame, "assets/bg_street_city.png", self.scaler, 0)
-        backgroundSecond = Entity(self.pygame, "assets/bg_street_city.png", 0, 0)
-        background.y = self.height - driveway.height * self.scaler - background.height * self.scaler
-        backgroundSecond.y = self.height - driveway.height * self.scaler - background.height * self.scaler
+        background = Entity(Game.pygame, "assets/bg_street_city.png", self.scaler, 0)
+        backgroundSecond = Entity(Game.pygame, "assets/bg_street_city.png", 0, 0)
+        background.y = self.height - driveway.height * self.scaler - background.height * self.scaler + self.scaler * 2
+        backgroundSecond.y = self.height - driveway.height * self.scaler - background.height * self.scaler + self.scaler * 2
 
         speed = 1
         distance = 0
-        my_font = self.pygame.font.SysFont('Berlin Sans FB', 24)
-        clock = self.pygame.time.Clock()
+        coins = 0
+        my_font = Game.pygame.font.Font('./assets/joystix.otf', 32)
+        clock = Game.pygame.time.Clock()
 
         while self.isRunning:
             clock.tick(60)
-            self.pygame.display.set_caption(f'{clock.get_fps() :.1f} fps')
-            for event in self.pygame.event.get():
-                if event.type == self.pygame.QUIT:
+            Game.pygame.display.set_caption(f'{clock.get_fps() :.1f} fps')
+            for event in Game.pygame.event.get():
+                if event.type == Game.pygame.QUIT:
                     self.isRunning = False
-            self.keys = self.pygame.key.get_pressed()
+            self.keys = Game.pygame.key.get_pressed()
             self.win.fill((0, 0, 0))
-            self.pygame.draw.rect(self.win, (85, 123, 153), (
+            Game.pygame.draw.rect(self.win, (85, 123, 153), (
                 self.windowBorder, self.windowBorder, self.width - 2 * self.windowBorder,
                 self.height - 2 * self.windowBorder))
 
             # for entity in self.entities:
             #     print(entity)
 
-            speed = speed * 1.002
+            speed = speed * 1.001
             distance = distance + speed
             self.drawEntity(background)
             self.moveEntity(driveway, -speed * self.scaler, -driveway.width * self.scaler, 0)
@@ -82,18 +100,26 @@ class Game:
             self.moveEntity(background, -speed * self.scaler, -background.width * self.scaler, 0)
             self.moveEntity(backgroundSecond, -speed * self.scaler, 0, backgroundSecond.width * self.scaler)
 
+            skate.x = skater.x
+            skate.y = skater.y + skater.height * self.scaler * 0.9
+            self.drawEntity(skate)
             self.drawEntity(skater)
+            self.keyhandlerMoveEntity(skater, 2, driveway)
 
-            self.drawText("Speed: " + str(round(speed, 2)).__str__(), my_font, (0, 0, 0), 24, 24)
-            self.drawText("Distance: " + distance.__trunc__().__str__(), my_font, (0, 0, 0), 24, 24*2)
-            self.drawText("Coins: null", my_font, (0, 0, 0), 24, 24*3)
+            self.drawText("Speed: " + str(round(speed, 2)).__str__(), my_font, (0, 0, 0), 16, 16)
+            self.drawText("Distance: " + distance.__trunc__().__str__(), my_font, (0, 0, 0), 16, 16 + 32)
+            self.drawText("Coins: " + coins.__str__(), my_font, (0, 0, 0), 16, 16 + 32 * 2)
+            self.drawText("PlayerX: " + skater.x.__str__(), my_font, (0, 0, 0), 16, 16 + 32 * 5)
+            self.drawText("PlayerY: " + skater.y.__str__(), my_font, (0, 0, 0), 16, 16 + 32 * 6)
 
-            if self.keys[self.pygame.K_ESCAPE]:
+            if self.keys[Game.pygame.K_ESCAPE]:
                 self.isRunning = False
-            if self.keys[self.pygame.K_UP]:
+            if self.keys[Game.pygame.K_UP]:
                 speed += .1
-            if self.keys[self.pygame.K_DOWN]:
+            if self.keys[Game.pygame.K_DOWN]:
                 speed -= .1
-            self.pygame.display.update()
+            if speed <= 0:
+                speed = 0
+            Game.pygame.display.update()
 
-        self.pygame.quit()
+        Game.pygame.quit()
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import pygame
-
 from game import Game
 
 windowWidth = 1024
 windowHeight = 768
 windowBorder = 0
 scaler = 2.5
-game = Game(pygame, scaler, windowWidth, windowHeight, False)
+game = Game(scaler, windowWidth, windowHeight, False)
 game.start()
